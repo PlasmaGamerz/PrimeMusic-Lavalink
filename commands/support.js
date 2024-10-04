50041a2c19386851ff264a93cf2b8bf197c9345a
PlasmaGamerz <146640311+PlasmaGamerz@users.noreply.github.com>
2024-10-04 19:07:45 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -31,14 +31,14 @@ module.exports = {
 
       const supportServerLink = "https://discord.com/invite/nYPKyTSZ";
       const githubLink = "https://github.com/PlasmaGamerz";
-      const replitLink = "https://replit.com/@Plasma Gamerz";
+      const replitLink = "https://replit.com/@Plasma_Gamerz";
       const youtubeLink = "https://youtube.com/@plasma_gamerz?si=Tx-cfqXLLu94THgG";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1272103065721241661/1291742153139159072/image.jpg?ex=67013420&is=66ffe2a0&hm=87475cf0ff12a8f3da9d71a481d1fec69724c3c12cbf412313035f1c3c401fdc&', 
-              url: 'https://discord.com/invite/nYPKyTSZ'
+              url: 'https://discord.gg/xQF9f9yUEM'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
